afa7c8ad3b1c736ac6e16f9afd4a83c40a6d4ce5
moraronelson-cloud <moraronelson@gmail.com>
2026-08-27 12:25:51 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='bl3-ration-v2';
+const CACHE='bl3-food-ration-v3';
 const CORE=['./','./index.html','./manifest.json'];
 self.addEventListener('install',event=>event.waitUntil(caches.open(CACHE).then(cache=>cache.addAll(CORE)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',event=>event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
